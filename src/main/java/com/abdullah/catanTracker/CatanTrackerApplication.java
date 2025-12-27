2b2ff47f50e516a644e9bd77fc7a96c83bbfef9d
Abdullah Khan <abkhan0517@gmail.com>
2025-12-27 12:10:24 -0500

    initial commit

---
+++ src/main/java/com/abdullah/catanTracker/CatanTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.abdullah.catanTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatanTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatanTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdullah/catanTracker/CatanTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullah.catanTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatanTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
